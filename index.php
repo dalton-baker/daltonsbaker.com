ec0710876c43d1b153514e6fd04a98de3bc1809e
Dalton Baker <dalton.s.baker@gmail.com>
2021-03-20 00:00:34 -0600

    Minor update to "coming soon" page.

--- index.php
+++ index.php
@@ -4,8 +4,28 @@
     <title>Dalton Baker's Portfolio</title>
 </head>
 
+<style>
+    body {
+        background-color: #121212;
+        font-size: 100%;
+        font-family: 'Roboto', sans-serif;
+        margin: 0;
+        color: white;
+    }
+
+    .message-text {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
 <body>
-<h1>Coming Soon!</h1>
+    <div class="message-text">
+        <h1>Coming Soon!</h1>
+    </div>
 </body>
 
 </html>
