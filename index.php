b1e5cdb8340565af1382f8a019c2e7735da75c49
Dalton Baker <dalton.s.baker@gmail.com>
2021-03-20 19:50:57 -0600

    Added basic site layout. Need to add content next.

--- index.php
+++ index.php
@@ -1,32 +1,50 @@
+<!DOCTYPE html>
 <html>
+<style>
+    <?php include 'index.css'; ?>
+</style>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dalton Baker's Portfolio</title>
 </head>
 
-<style>
-    body {
-        background-color: #121212;
-        font-size: 100%;
-        font-family: 'Roboto', sans-serif;
-        margin: 0;
-        color: white;
-    }
-
-    .message-text {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-</style>
-
 <body>
-    <div class="message-text">
-        <h1>Coming Soon!</h1>
-        <h2>Email dalton.s.baker@gmail.com with questions</h2>
+    <div class="title-div">
+        <div class="title">Dalton Baker</div>
+        <div class="sub-title">Computer Science B.S. at SDSM&T</div>
+    </div>
+
+    <div id="portfolio">
+        <input type="radio" name="menu_toggle" id="repository-tog">
+        <input type="radio" name="menu_toggle" id="example-tog">
+        <input type="radio" name="menu_toggle" id="contact-tog">
+
+        <div id="menu-bar">
+            <label for="repository-tog" id="repository-label" tabindex="1">Git Repositories</label>
+            <label for="example-tog" id="example-label" tabindex="2">Examples Of Work</label>
+            <label for="contact-tog" id="contact-label" tabindex="3">Contact Info</label>
+        </div>
+
+        <div id="menu-content">
+            <div id="default-content">
+                Under Construction.
+                <!-- <p>This is Dalton Baker's portfolio</p>
+                <p>If you need to contact me, please see the contact info tab.</p> -->
+            </div>
+
+            <div id="repository-content">
+                Under Construction.
+            </div>
+
+            <div id="example-content">
+                Under Construction.
+            </div>
+
+            <div id="contact-content">
+                Under Construction.
+            </div>
+        </div>
     </div>
 </body>
 
