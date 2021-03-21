5ef18344449bac973e8c109cff01556897bcc20a
Dalton Baker <dalton.s.baker@gmail.com>
2021-03-20 21:12:25 -0600

    Added content to each tab. Looks ok but needs work

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 </style>
 
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dalton Baker's Portfolio</title>
 </head>
@@ -28,21 +29,41 @@
 
         <div id="menu-content">
             <div id="default-content">
-                Under Construction.
-                <!-- <p>This is Dalton Baker's portfolio</p>
-                <p>If you need to contact me, please see the contact info tab.</p> -->
+                <p>Welcome to my portfolio</p>
             </div>
 
             <div id="repository-content">
-                Under Construction.
+                <a href="https://gitlab.com/dalton_baker" id="gitlab-link">
+                    <img src="./assets/gitlab.jpg">
+                    <div><i class="fa fa-gitlab"></i>&nbsp; Gitlab</div>
+                </a>
+                <a href="https://github.com/dalton-baker" id="github-link">
+                    <img src="./assets/github.jpg">
+                    <div><i class="fa fa-github"></i>&nbsp; Github</div>
+                </a>
             </div>
 
             <div id="example-content">
-                Under Construction.
+                <a href="https://metalsmarketdisplay.com" id="metalsmarket-link">
+                    <img src="./assets/metalsmarketdisplay.jpg">
+                    <div>metalsmarketdisplay.com</div>
+                </a>
+                <a href="https://dakotahstaxes.com/" id="github-link">
+                    <img src="./assets/dakotahtaxes.jpg">
+                    <div>dakotahstaxes.com</div>
+                </a>
             </div>
 
             <div id="contact-content">
-                Under Construction.
+                <a href="https://www.linkedin.com/in/dalton-baker/" id="normal-link">
+                    <div><i class="fa fa-linkedin"></i>&nbsp; LinkdIn</div>
+                </a>
+            </div>
+
+            <div id="contact-content">
+                <a href="mailto: dalton.s.baker@gmail.com" id="normal-link">
+                    <div><i class="fa fa-google"></i>&nbsp; dalton.s.baker@gmail.com</div>
+                </a>
             </div>
         </div>
     </div>
